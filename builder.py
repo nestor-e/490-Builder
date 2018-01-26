77e5b84ae2594f6a9fe7671c81d69d0299315e78
Edward Nestor <tnblend@gmail.com>
2018-01-25 17:09:32 -0800

    first working version, needs clean up and documentation

--- builder.py
+++ builder.py
@@ -1,30 +1,19 @@
 import networkx as nwx
 import dbWrapper
+import dbWrapperCSV as dbc
 
 
 class GraphBuilder:
 
     def __init__(self, dbSource):
-        self.db = dbSource
-        self.names = None
-        self.tabs = None
-        self.nEdges = None
-        self.tEdges = None
-        self.vertsBuilt = False
-        self.tEdgesBuilt = False
-        self.nEdgesBuilt = False
-
-    def _buildVertex(self):
-        if not self.vertsBuilt:
-            self.names  = self.db.attestationTableByName()
-            self.tabs = self.db.attestationTableByTablet()
-            if (not self.names) and (not self.tabs):
-                print "Data source not sufficent"
-            elif not self.names:
-                self.names = inferOccurances(self.tabs)
-            elif not self.tabs:
-                self.tabs = inferOccurances(self.names)
-            self.vertsBuilt = True
+        self._db = dbSource
+        self._names = None
+        self._tabs = None
+        self._nEdges = None
+        self._tEdges = None
+        self._vertsBuilt = False
+        self._tEdgesBuilt = False
+        self._nEdgesBuilt = False
 
     @staticmethod
     def _inferOccurances(other):
@@ -38,38 +27,60 @@ class GraphBuilder:
                     occ[ref] = [key]
         return occ
 
+    def _buildVertex(self):
+        if not self._vertsBuilt:
+            self._names  = self._db.attestationTableByName()
+            self._tabs = self._db.attestationTableByTablet()
+            if (not self._names) and (not self._tabs):
+                print "Data source not sufficent"
+                return
+            elif not self._names:
+                self._names = self._inferOccurances(self._tabs)
+            elif not self._tabs:
+                self._tabs = self._inferOccurances(self._names)
+            self._vertsBuilt = True
+
+
+
     def _buildTabEdges(self):
-        if self.vertsBuilt and not self.tEdgesBuilt:
-            self.tEdges = self.buildEdges(self.tabs, self.names)
-            self.tEdgesBuilt = True
+        if self._vertsBuilt and not self._tEdgesBuilt:
+            self._tEdges = self._buildEdges(self._tabs, self._names)
+            self._tEdgesBuilt = True
 
     def _buildNameEdges(self):
-        if self.vertsBuilt and not self.nEdgesBuilt:
-            self.tEdges = self.buildEdges(self.names, self.tabs)
-            self.nEdgesBuilt = True
+        if self._vertsBuilt and not self._nEdgesBuilt:
+            self._nEdges = self._buildEdges(self._names, self._tabs)
+            self._nEdgesBuilt = True
 
     def _buildEdges(self, verts, other):
         edges = {}
         for v1 in verts:
             for ref in verts[v1]:
                 for v2 in other[ref]:
-                    self.updateEdge(v1, v2, edges)
+                    self._updateEdge(v1, v2, ref, edges)
         return edges
 
     @staticmethod
-    def _updateEdge(v1, v2, edgeSet):
+    def _updateEdge(v1, v2, link, edgeSet):
         if v1 < v2:
             key = (v1, v2)
             if key in edgeSet:
-                edgeSet[key] += 1
+                edgeSet[key]['weight'] += 1
+                edgeSet[key]['linkedBy'].append(link)
             else:
-                edgeSet[key] = 1
+                edgeSet[key] = {'weight' : 1, 'linkedBy' : [link]}
 
-    def _buildGraph(self, verts, edges, useWeights):
+    def _buildGraph(self, verts, edges, useWeights, keepEdgeLabels):
         vSet = [vId for vId in verts]
         eSet = []
-        if useWeights:
-            eSet = [ (edge[0], edge[1], {'weight' : edges[edge]}) for edge in edges]
+        if useWeights or keepEdgeLabels:
+            for edge in edges:
+                edgeData = {}
+                if useWeights:
+                    edgeData['weight'] = edges[edge]['weight']
+                if keepEdgeLabels:
+                    edgeData['linkedBy'] = edges[edge]['linkedBy']
+                eSet.append( ( edge[0], edge[1], edgeData))
         else:
             eSet = [ edge for edge in edges]
         g = nwx.Graph()
@@ -77,27 +88,27 @@ class GraphBuilder:
         g.add_edges_from(eSet)
         return g
 
-    def getTabletGraph(self, useWeights = True):
-        self.buildVertex()
-        self.buildTabEdges()
-        if not (self.nTabBuilt and self.vertsBuilt):
+    def getTabletGraph(self, useWeights = True, keepEdgeLabels = False):
+        self._buildVertex()
+        self._buildTabEdges()
+        if not (self._tEdgesBuilt and self._vertsBuilt):
             print "Error: could not build graph"
             return None
-        return self.buildGraph(self.tabs, self.tEdges, useWeights)
+        return self._buildGraph(self._tabs, self._tEdges, useWeights, keepEdgeLabels)
 
 
-    def getNameGraph(self, useWeights = True):
-        self.buildVertex()
-        self.buildNameEdges()
-        if not (self.nEdgesBuilt and self.vertsBuilt):
+    def getNameGraph(self, useWeights = True, keepEdgeLabels = False):
+        self._buildVertex()
+        self._buildNameEdges()
+        if not (self._nEdgesBuilt and self._vertsBuilt):
             print "Error: could not build graph"
             return None
-        return self.buildGraph(self.names, self.nEdges, useWeights)
+        return self._buildGraph(self._names, self._nEdges, useWeights, keepEdgeLabels)
 
 def main():
-    db = dbWrapper.dbWrapper()
+    db = dbc.CsvWrapper('testGraph.csv')
     gb = GraphBuilder(db)
-    gb.getTabletGraph()
+    G = gb.getTabletGraph()
 
 
 if __name__ == "__main__":
